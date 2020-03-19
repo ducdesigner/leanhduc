de4a25b4fb0f1736880c6f2bb5d5cef8cd9ae2c9
Anh Duc <leanhducdes@gmail.com>
2020-03-19 23:17:27 +0700

    upload lai sau khi lam isotope

--- 1.js
+++ 1.js
@@ -1,4 +1,13 @@
 ﻿ $(function(){
- 
+ 	
+ 	$hieuung = $('.noidung').isotope({
+	  	// options
+	  	itemSelector: '.khoianh',
+	  	layoutMode: 'masonry'
+	});
+
+	$hieuung.imagesLoaded().progress( function() {
+  		$hieuung.isotope('layout');
+  	});
 })  
  
